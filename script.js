6ea36f8f7400806641050bac2d22313ccfe01248
Vaqif Mammadov <80715010+vag1ffm@users.noreply.github.com>
2022-09-27 10:35:04 +0400

    Updates

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function hide() {
-    let a = document.querySelector('.first-infa')
-    let b = document.querySelector('.second-infa')
-    let c = document.querySelector('.third-infa')
-    a.style.display = 'none'
-    b.style.display = 'none'
-    c.style.display = 'none'
+    document.querySelector('.first-infa').style.display = 'none'
+    document.querySelector('.second-infa').style.display = 'none'
+    document.querySelector('.third-infa').style.display = 'none'
+
 }
 
 
@@ -22,15 +20,16 @@ function poisk() {
     let cityName =  document.querySelector('.poiskovik label input:first-child').value
     const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
     fetch(url).then(data => data.json())
-         // .then(console.log)
         .then(data2 => {
             cityName = data2.name;
+        
             //first-info
             document.querySelector('.f-qradus > h2').innerText = `${Math.round(data2.main.temp - 274)}`
             document.querySelector('.f-qorod > p').innerText = `${cityName}`
             let iconcod = data2.weather[0].icon
             let iconurl =  "http://openweathermap.org/img/w/" + iconcod + ".png";
             imgofpoqoda.src = iconurl
+        
             //second-info
             document.querySelector('.s-qorod > h2').innerText = `${cityName}`
             document.querySelector('.tempr > p').innerText = `${Math.round(data2.main.temp - 274)}`
@@ -40,9 +39,6 @@ function poisk() {
             let sunsettime = new Date(data2.sys.sunset*1000- (14400-data2.timezone)*1000)
             document.querySelector('.vosxod > p').innerText = `${sunrisetime.getHours()}:${sunrisetime.getMinutes()}`
             document.querySelector('.zakat > p').innerText = `${sunsettime.getHours()}:${sunsettime.getMinutes()}`
-
-
-            console.log(data2)
         }).catch(err => alert('Нет такого города-_-'))
 
 }
@@ -56,7 +52,7 @@ window.addEventListener('keyup', (event) => {
             poisk()
         }
         show('first-infa')
-        // iconofpoqoda.innerHTML = ''
+       
     }
 })
 
@@ -66,15 +62,11 @@ function like(){
     let count = document.querySelectorAll('.added-city>ul>li')
     let list = [];
     count.forEach(li =>{ list.push(li.innerText)})
-    if (list.includes(a)) {
-
-    } else {
+    if (!list.includes(a)) {
         let place = document.querySelector('.added-city>ul')
         place.innerHTML += `<li onclick="FastShow('${a}')">${a}</li>`
         localStorage.setItem(list.length, a)
-    }
-
-
+    } 
 }
 
 function FastShow(i) {
@@ -86,23 +78,12 @@ function FastShow(i) {
 function forlocalstor() {
     for (let i = 0; i<localStorage.length ;i++) {
         let key = localStorage.key(i)
-        let a = localStorage.getItem(key)
+        let value = localStorage.getItem(key)
         let place = document.querySelector('.added-city>ul')
-        let b = a;
-        place.innerHTML += `<li onclick="FastShow('${b}')">${a}</li>`
+        let liEliment = value;
+        place.innerHTML += `<li onclick="FastShow('${liEliment}')">${value}</li>`
 
     }
 }
 
 forlocalstor()
-// localStorage.clear()
-
-
-
-
-
-
-
-
-
-
